aad9cf8d26d0608f15bae422db182259c60a715c
Kuba Groblewski <kkgroblewski@gmail.com>
2024-03-30 21:54:15 +0100

    feat: buehlmann model integration tests separated

--- src/buehlmann/buehlmann_model.rs
+++ src/buehlmann/buehlmann_model.rs
@@ -165,7 +165,6 @@ impl BuehlmannModel {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::common::Gas;
 
     #[test]
     fn test_state() {
@@ -175,94 +174,5 @@ mod tests {
         model.step(&10., &(10 * 60), &air);
         model.step(&15., &(15 * 60), &nx32);
         assert_eq!(model.state, ModelState { depth: 15., time: (25 * 60), gas: nx32 });
-    }
-
-    #[test]
-    fn test_ceiling() {
-        let mut model = BuehlmannModel::new(BuehlmannConfig::default());
-        let air = Gas::new(0.21, 0.);
-        model.step(&40., &(30 * 60), &air);
-        model.step(&30., &(30 * 60), &air);
-        let calculated_ceiling = model.ceiling();
-        assert_eq!(calculated_ceiling, 7.860647737614171);
-    }
-
-    #[test]
-    fn test_gfs() {
-        let mut model = BuehlmannModel::new(BuehlmannConfig::default());
-        let air = Gas::new(0.21, 0.);
-
-        model.step(&50., &(20 * 60), &air);
-        assert_eq!(model.gfs_current(), (0., 195.48223043242453));
-
-        model.step(&40., &(10 * 60), &air);
-        assert_eq!(model.gfs_current(), (0., 210.41983141337982));
-    }
-
-    #[test]
-    fn test_model_steps_equality() {
-        let mut model1 = BuehlmannModel::new(BuehlmannConfig::default());
-        let mut model2 = BuehlmannModel::new(BuehlmannConfig::default());
-
-        let air = Gas::new(0.21, 0.);
-        let test_depth = 50.;
-        let test_time_minutes: usize = 100;
-
-        model1.step(&test_depth, &(test_time_minutes * 60), &air);
-
-        // step every second
-        for _i in 1..=(test_time_minutes * 60) {
-            model2.step(&test_depth, &1, &air);
-        }
-
-        assert_eq!(model1.ceiling().floor(), model2.ceiling().floor());
-
-        let (model1_gf_now, model1_gf_surf) = model1.gfs_current();
-        let (model2_gf_now, model2_gf_surf) = model1.gfs_current();
-        assert_eq!(model1_gf_now.floor(), model2_gf_now.floor());
-        assert_eq!(model1_gf_surf.floor(), model2_gf_surf.floor());
-    }
-
-    #[test]
-    fn test_ndl_calculation() {
-        let mut model = BuehlmannModel::new(BuehlmannConfig::default());
-        let air = Gas::new(0.21, 0.);
-        let depth = 30.;
-
-        // with 21/00 at 30m expect NDL 16
-        model.step(&depth, &0, &air);
-        assert_eq!(model.ndl(), 16);
-
-        // expect NDL 15 after 1 min
-        model.step(&depth, &(1*60), &air);
-        assert_eq!(model.ndl(), 15);
-    }
-
-    #[test]
-    fn test_ndl_cut_off() {
-        let mut model = BuehlmannModel::new(BuehlmannConfig::default());
-        let air = Gas::new(0.21, 0.);
-
-        model.step(&0., &0, &air);
-        assert_eq!(model.ndl(), Minutes::MAX);
-
-        model.step(&10., &(10*60), &air);
-        assert_eq!(model.ndl(), Minutes::MAX);
-    }
-
-    #[test]
-    fn test_multi_gas_ndl() {
-        let mut model = BuehlmannModel::new(BuehlmannConfig::default());
-        let air = Gas::new(0.21, 0.);
-        let ean_28 = Gas::new(0.28, 0.);
-
-        model.step(&30., &(0 * 60), &air);
-        assert_eq!(model.ndl(), 16);
-
-        model.step(&30., &(10 * 60), &air);
-        assert_eq!(model.ndl(), 6);
-
-        model.step(&30., &(0 * 60), &ean_28);
-        assert_eq!(model.ndl(), 9);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 mod common;
 mod buehlmann;
 
-pub use common::{DecoModel, Gas, Depth, Seconds, Pressure};
+pub use common::{DecoModel, Gas, Depth, Minutes, Seconds, Pressure};
 
 pub use buehlmann::{BuehlmannModel, BuehlmannConfig};
