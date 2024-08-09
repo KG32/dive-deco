1e3ed7723a2e1af99ac5c7bdeadacd6819cd3264
Kuba Groblewski <kkgroblewski@gmail.com>
2024-08-10 00:01:25 +0200

    buehlmann benchmarks init

---
+++ benches/buehlmann_benchmark.rs
@@ -0,0 +1,27 @@
+use criterion::{ Criterion, criterion_group, criterion_main };
+use dive_deco::{ BuehlmannModel, DecoModel, Gas };
+
+pub fn buehlmann_ndl_benchmark(c: &mut Criterion) {
+    c.bench_function("Buehlmann NDL", |b| b.iter(|| {
+        let mut model = BuehlmannModel::default();
+        model.step(20., 5, &Gas::air());
+        model.ndl();
+    }));
+}
+
+pub fn buehlmann_deco_benchmark(c: &mut Criterion) {
+    let mut model = BuehlmannModel::default();
+    let air = Gas::new(0.21, 0.);
+    let ean_50 = Gas::new(0.50, 0.);
+    model.step(40.0001, 20 * 60, &air);
+    c.bench_function("Buehlmann deco", |b| b.iter(|| {
+        model.deco(vec![air, ean_50])
+    }));
+}
+
+criterion_group!(
+    benches,
+    buehlmann_ndl_benchmark,
+    buehlmann_deco_benchmark
+);
+criterion_main!(benches);
